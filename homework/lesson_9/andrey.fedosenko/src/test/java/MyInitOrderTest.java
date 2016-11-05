43aa5b43ae2f5a616d5bee94af2163ab4688ebdc
Andrii Fedosenko <oramazd@gmail.com>
2016-11-05 19:02:14 +0200

    Andrey Fedosenko: homework 9. Add own test.

--- homework/lesson_9/andrey.fedosenko/src/test/java/MyInitOrderTest.java
+++ homework/lesson_9/andrey.fedosenko/src/test/java/MyInitOrderTest.java
@@ -1,15 +1,19 @@
 import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import exersice.TopLevelClass;
+import exersice.logger.InitOrderLogger;
 import exersice.GrandParentClass;
 import exersice.ParentClass;
-import exersice.TopLevelClass;
 
 public class MyInitOrderTest {
   @Test
   public void checkClassesCostructors() throws Exception {
-    new GrandParentClass();
-    new ParentClass();
-    new TopLevelClass();
+    TopLevelClass tp = new TopLevelClass();
+    assertNotNull(tp);
+    assertTrue(InitOrderLogger.getInstance().getLogString().equals("init_order: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15"));
+    assertNotNull(new GrandParentClass());
+    assertNotNull(new ParentClass());
   }
-
 }
